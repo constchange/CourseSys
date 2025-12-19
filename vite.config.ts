585a7c05e4dc1755f846d80b3632b507596d5ca1
constchange <414670645@qq.com>
2025-12-19 12:30:57 +0800

    Modify setting to deploy on GitHub

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  
+  // 👇 添加这一行！
+  // 这里的 '你的仓库名' 必须和 GitHub 上的仓库名一模一样，前后都要加斜杠
+  base: '/CourseSys/', 
+
 })
